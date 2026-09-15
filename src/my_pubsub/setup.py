1ed745cd7441b983419ed8847a36be077df68ab7
minsoo3066 <98580113+minsoo3066@users.noreply.github.com>
2026-09-15 16:47:19 +0900

    Add launch file installation to package

--- src/my_pubsub/setup.py
+++ src/my_pubsub/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'my_pubsub'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'),
+            glob('launch/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
