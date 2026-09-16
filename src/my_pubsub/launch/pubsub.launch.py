f3ac8b3f76d91bd336e9272a352be87b82d914d1
minsoo3066 <98580113+minsoo3066@users.noreply.github.com>
2026-09-16 16:48:16 +0900

    Complete Launch namespace and multi-robot practice

--- src/my_pubsub/launch/pubsub.launch.py
+++ src/my_pubsub/launch/pubsub.launch.py
@@ -4,19 +4,63 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    publisher_node = Node(
+    robot1_publisher = Node(
         package='my_pubsub',
         executable='publisher',
-        output='screen'
+        name='launch_publisher',
+        namespace='robot1',
+        output='screen',
+        parameters=[
+            {
+                'timer_period': 1.0
+            }
+        ],
+        remappings=[
+            ('chatter', 'robot_message')
+        ]
     )
 
-    subscriber_node = Node(
+    robot1_subscriber = Node(
         package='my_pubsub',
         executable='subscriber',
-        output='screen'
+        name='launch_subscriber',
+        namespace='robot1',
+        output='screen',
+        remappings=[
+            ('chatter', 'robot_message')
+        ]
+    )
+
+    robot2_publisher = Node(
+        package='my_pubsub',
+        executable='publisher',
+        name='launch_publisher',
+        namespace='robot2',
+        output='screen',
+        parameters=[
+            {
+                'timer_period': 2.0
+            }
+        ],
+        remappings=[
+            ('chatter', 'robot_message')
+        ]
+    )
+
+    robot2_subscriber = Node(
+        package='my_pubsub',
+        executable='subscriber',
+        name='launch_subscriber',
+        namespace='robot2',
+        output='screen',
+        remappings=[
+            ('chatter', 'robot_message')
+        ]
     )
 
     return LaunchDescription([
-        publisher_node,
-        subscriber_node
+        robot1_publisher,
+        robot1_subscriber,
+        robot2_publisher,
+        robot2_subscriber
     ])
